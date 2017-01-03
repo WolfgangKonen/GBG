b741a12a7bd3913a480345ec45d22b6db32e340b
Johannes-Kutsch <johanneskutsch@live.de>
2017-01-03 13:19:32 +0100

    Revert "."
    This reverts commit 44cb5f0b9421b7ad526e3c8c377634bfcf51e701.

--- src/games/ZweiTausendAchtundVierzig/Config.java
+++ src/games/ZweiTausendAchtundVierzig/Config.java
@@ -12,15 +12,4 @@ public class Config {
     public static final int STARTINGVALUES[] = {2,2,2,2,2,2,2,2,2,4};
 
     public static final int NUMBEREVALUATIONS = 100;
-
-    //Heuristik Values
-    public static final double EMPTYTILEVALUE = 1.03;
-    public static final double HIGHESTTILEINCORENERVALUE = 1.75;
-
-    //Bestrafung
-    //Value from -1 to 0
-    public static final double PENALISATION = -1;
-    //Gibt an ob die unveränderte Gamescore zu der Bestrafung hinzuaddiert werden soll. Heuristiken werden bei dieser Socre nich berücksichtigt!
-    public static final boolean ADDSCORE = true;
-
 }
