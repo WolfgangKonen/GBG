ad1747b680722f8a192d96f23085542a52070ca5
Felix Barsnick <felix_barsnick@hotmail.de>
2018-05-29 12:04:28 +0200

    notes und planung TS

---
+++ src/tmp/notes.java
@@ -0,0 +1,32 @@
+package tmp;
+
+import controllers.AgentBase;
+import games.Arena;
+import games.TicTacToe.LaunchTrainTTT;
+import games.XArenaMenu;
+
+public class notes
+{
+    public static void main(String[] args)
+    {
+        /**
+         *
+         * meine wichtigen Klassen
+         *
+         * Kette der Vererbung/Aufrufe:
+         * @see LaunchTrainTTT // Startpunkt TTT
+         * @see games.TicTacToe.ArenaTrainTTT
+         * @see games.ArenaTrain
+         * @see Arena // Arenacode und Gameloop
+         *
+         * @see XArenaMenu // TopMenu der GUI
+         *
+         * @see AgentBase // abstrakte Grundlagenklasse aller Agenten
+         *
+         * @see tools.Types.GUI_SCALING_FACTOR // Faktor zur GUI skalierung | hier noch nicht verfügbar da noch nicht gemergt
+         *
+         * @see games.LogManagerGUI // bsp gui im eigenen Fenster - vorlage TS GUI?
+         *
+         */
+    }
+}
