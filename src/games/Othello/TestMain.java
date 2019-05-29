bbb932d839cb88d563c7605053b41fbd6f522b41
Thot38 <yannick.yd8@gmail.com>
2019-05-29 20:54:16 +0200

    ! redid the CommandLineInteractor + to comprehend the CommandLineInteractor I included a very simple Test.exe ! the implementation of Edax will follow soon!

--- src/games/Othello/TestMain.java
+++ src/games/Othello/TestMain.java
@@ -1,13 +1,5 @@
 package games.Othello;
 
-import java.io.File;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import games.Othello.Edax.CommandLineInteractor;
 
 public class TestMain {
@@ -16,80 +8,21 @@ public class TestMain {
 	{		
 		System.out.flush();
 		/**
-		 * New Fancy Way
+		 * The First test with my test.exe to test timing behaviours and timeouts
 		 */
-		CommandLineInteractor cli = new CommandLineInteractor("agents\\Othello\\Edax", "edax.exe", ".*[eE]dax plays ([A-z][0-8]).*", 1);
-		new Thread(cli).start();
-		System.out.println(cli.doAction("mode 1"));
-		System.out.println(cli.doAction("f4"));
-		System.out.println(cli.doAction("f6"));
+		CommandLineInteractor cli = new CommandLineInteractor("agents\\Othello\\Edax", "Test.exe");
+		System.out.println(cli.sendAndAwait("mode 1"));
+		System.out.println(cli.sendAndAwait("10000"));
+		cli.sendAndAwait("f");
 		cli.stop();
 
-		/*Old Way to do stuff, just for reference*/
 		/**
-		Process p = null;
-		System.out.flush();
-		try {
-			ProcessBuilder pb = new ProcessBuilder();
-			pb.command("cmd.exe", "/c", "edax.exe");
-			pb.directory(new File("agents\\Othello\\Edax"));
-			p = pb.start();
-			new Thread(new SyncPipe(p.getErrorStream(), System.err), System.currentTimeMillis() + "").start();
-		    new Thread(new SyncPipe(p.getInputStream(), System.out), System.currentTimeMillis() + "").start();
-
-		    PrintWriter pw = new PrintWriter(p.getOutputStream(), true);
-
-		    pw.println("mode 1");
-		    Thread.sleep(250);
-		    for(int i = 0; i < 7; i++)
-		    {
-		    	pw.println("f4");
-		    	Thread.sleep(250);		    	
-		    }
-//		    pw.println("f6");
-//		    Thread.sleep(250);
-//		    pw.println("c4");
-//		    Thread.sleep(250);
-		    pw.close();
-		    p.waitFor();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		finally {
-			p.destroyForcibly();
-		}
-	}	
-
-	public static class SyncPipe implements Runnable
-	{
-	public SyncPipe(InputStream istrm, OutputStream ostrm) {
-	      istrm_ = istrm;
-	      ostrm_ = ostrm;
-	  }
-	  public void run() {
-		  try
-	      {
-	          final byte[] buffer = new byte[1024];
-	          for (int length = 0; (length = istrm_.read(buffer)) != -1; )
-	          {
-//	              ostrm_.write(buffer, 0, length);
-	        	  String str = new String(buffer, StandardCharsets.UTF_8);
-	        	  Pattern test = Pattern.compile(".*[eE]dax plays ([A-z][0-8]).*");
-
-	        	  Matcher m = test.matcher(str);
-	        	  if(m.find())
-	        		  System.out.println(m.group(1));
-	          }
-	      }
-	      catch (Exception e)
-	      {
-	          e.printStackTrace();
-	      }
-	  }
-	  private final OutputStream ostrm_;
-	  private final InputStream istrm_;
+		 * The second test with edax.exe, to test the first three moves
+		 */
+//		CommandLineInteractor cli = new CommandLineInteractor("agents\\Othello\\Edax", "edax.exe", ".*[eE]dax plays ([A-z][0-8]).*", 1);
+//		System.out.println(cli.sendAndAwait("mode 1"));
+//		System.out.println(cli.sendAndAwait("f6"));
+//		System.out.println(cli.sendAndAwait("f4"));
+//		cli.stop();
 	}
-*/
-	}	
 }
