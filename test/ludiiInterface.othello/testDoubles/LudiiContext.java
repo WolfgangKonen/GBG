62f1a569ec5cbe66aaea9588eddcce99ea3c27ed
Johannes Scheiermann <johannes.scheiermann@live.de>
2020-07-03 23:50:56 +0200

    Add Moves use case with unit tests

--- test/ludiiInterface.othello/testDoubles/LudiiContext.java
+++ test/ludiiInterface.othello/testDoubles/LudiiContext.java
@@ -1,48 +1,49 @@
 package ludiiInterface.othello.testDoubles;
 
-import game.rules.play.moves.Moves;
-import util.Context;
+import ludiiInterface.othello.transform.boardIndices.LudiiBoardIndex;
+import ludiiInterface.othello.useCases.moves.LudiiMoves;
 import player.utils.loading.GameLoader;
+import util.Context;
 import util.Move;
 import util.Trial;
-import util.action.Action;
+import util.action.move.ActionMove;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Optional;
 
 public final class LudiiContext {
     private static final String _gameName = "Reversi.lud";
 
     public static Context fromMoves(final List<Integer> ludiiIndices) {
-        final var game = GameLoader.loadGameFromName(_gameName);
-        final var context = new Context(game, new Trial(game));
-        game.start(context);
+        final var context = newGame();
+        context.game().start(context);
 
-        ludiiIndices.stream().forEach(i -> game.apply(context, moveToIndex(game.moves(context), i)));
+        ludiiIndices.forEach(i -> tryApplyMoveTo(context, i));
 
         return context;
+    }
+
+    private static Context newGame() {
+        final var game = GameLoader.loadGameFromName(_gameName);
+        return new Context(game, new Trial(game));
     }
 
     public static Context forNewGame() {
         return fromMoves(new ArrayList<>());
     }
 
-    private static Move moveToIndex(final Moves moves, final int toIndex) {
-        return Arrays
-            .stream(
-                moves.moves().toArray(new Move[0])
-            ).filter(m -> actionsFor(m).filter(a -> a.locnB().get(0) == toIndex).count() > 0).findFirst().get();
+    private static void tryApplyMoveTo(final Context context, final int toIndex) {
+        moveToIndex(context, toIndex)
+            .ifPresent(
+                move -> context.game().apply(context, move)
+            );
     }
 
-    private static Stream<Action> actionsFor(final Move move) {
-        return move.actions()
-            .stream()
-            .flatMap(
-                a -> a instanceof Move
-                    ? actionsFor((Move) a)
-                    : Arrays.stream(new Action[]{a})
+    private static Optional<Move> moveToIndex(final Context context, final int toIndex) {
+        return new LudiiMoves(context)
+            .availableMoveBy(
+                new LudiiBoardIndex(new ActionMove(toIndex, toIndex))
             );
     }
 }
---
+++ test/ludiiInterface.othello/useCases/moves/LudiiMovesTest.java
@@ -0,0 +1,187 @@
+package ludiiInterface.othello.useCases.moves;
+
+import com.nitorcreations.junit.runners.NestedRunner;
+import ludiiInterface.othello.testDoubles.LudiiContext;
+import ludiiInterface.othello.transform.boardIndices.GbgBoardIndex;
+import ludiiInterface.othello.transform.boardIndices.LudiiBoardIndex;
+import ludiiInterface.othello.transform.players.LudiiPlayer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import tools.Types;
+import util.action.move.ActionMove;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+@RunWith(NestedRunner.class)
+public final class LudiiMovesTest {
+    private static boolean MoveIsFromPlayer(
+        final Map.Entry<LudiiBoardIndex, LudiiPlayer> boardIndexPlayerMap,
+        final int player
+    ) {
+        return boardIndexPlayerMap.getValue().toInt() == player;
+    }
+
+    private static boolean MoveIsToBoardIndex(
+        final Map.Entry<LudiiBoardIndex, LudiiPlayer> boardIndexPlayerMap,
+        final int boardIndex
+    ) {
+        return boardIndexPlayerMap.getKey().toInt() == boardIndex;
+    }
+
+    public static final class A_new_game_is_already_starting_with {
+        @Test
+        public void four_played_moves() {
+            assertEquals(
+                4,
+                new LudiiMoves(LudiiContext.forNewGame())
+                    .played().size()
+            );
+        }
+
+        @Test
+        public void player_1_at_position_36() {
+            assertTrue(
+                new LudiiMoves(LudiiContext.forNewGame())
+                    .played().entrySet().stream()
+                    .anyMatch(
+                        m -> MoveIsFromPlayer(m, 1)
+                            && MoveIsToBoardIndex(m, 36)
+                    )
+            );
+        }
+
+        @Test
+        public void player_1_at_position_27() {
+            assertTrue(
+                new LudiiMoves(LudiiContext.forNewGame())
+                    .played().entrySet().stream()
+                    .anyMatch(
+                        m -> MoveIsFromPlayer(m, 1)
+                            && MoveIsToBoardIndex(m, 27)
+                    )
+            );
+        }
+
+        @Test
+        public void player_2_at_position_35() {
+            assertTrue(
+                new LudiiMoves(LudiiContext.forNewGame())
+                    .played().entrySet().stream()
+                    .anyMatch(
+                        m -> MoveIsFromPlayer(m, 2)
+                            && MoveIsToBoardIndex(m, 35)
+                    )
+            );
+        }
+
+        @Test
+        public void player_2_at_position_28() {
+            assertTrue(
+                new LudiiMoves(LudiiContext.forNewGame())
+                    .played().entrySet().stream()
+                    .anyMatch(
+                        m -> MoveIsFromPlayer(m, 2)
+                            && MoveIsToBoardIndex(m, 28)
+                    )
+            );
+        }
+    }
+
+    public static final class The {
+        @Test
+        public void played_moves_are_sorted_chronologically() {
+            final var playedMoves = new ArrayList<>(
+                new LudiiMoves(LudiiContext.fromMoves(
+                    List.of(43, 42)
+                )).played().entrySet());
+
+            final var secondToLastMove = playedMoves.get(4);
+            assertTrue(
+                MoveIsFromPlayer(secondToLastMove, 1) &&
+                    MoveIsToBoardIndex(secondToLastMove, 43)
+            );
+
+            final var lastMove = playedMoves.get(5);
+            assertTrue(
+                MoveIsFromPlayer(lastMove, 2) &&
+                    MoveIsToBoardIndex(lastMove, 42)
+            );
+        }
+
+        public static final class Method_availableMoveBy_only_returns_a_value_if_there {
+            public static final class Is_an_available_move_in_this_round_that_corresponds_to_the_passed {
+                @Test
+                public void gbgAction() {
+                    assertTrue(
+                        new LudiiMoves(LudiiContext.forNewGame())
+                            .availableMoveBy(
+                                new Types.ACTIONS(19)
+                            )
+                            .isPresent()
+                    );
+                }
+
+                @Test
+                public void gbgBoardIndex() {
+                    assertTrue(
+                        new LudiiMoves(LudiiContext.forNewGame())
+                            .availableMoveBy(
+                                new GbgBoardIndex(new Types.ACTIONS(19))
+                            )
+                            .isPresent()
+                    );
+                }
+
+                @Test
+                public void ludiiBoardIndex() {
+                    assertTrue(
+                        new LudiiMoves(LudiiContext.forNewGame())
+                            .availableMoveBy(
+                                new LudiiBoardIndex(new ActionMove(43, 43))
+                            )
+                            .isPresent()
+                    );
+                }
+            }
+
+            public static final class Is_not_an_available_move_in_this_round_that_corresponds_to_the_passed {
+                @Test
+                public void gbgAction() {
+                    assertFalse(
+                        new LudiiMoves(LudiiContext.forNewGame())
+                            .availableMoveBy(
+                                new Types.ACTIONS(11)
+                            )
+                            .isPresent()
+                    );
+                }
+
+                @Test
+                public void gbgBoardIndex() {
+                    assertFalse(
+                        new LudiiMoves(LudiiContext.forNewGame())
+                            .availableMoveBy(
+                                new GbgBoardIndex(new Types.ACTIONS(11))
+                            )
+                            .isPresent()
+                    );
+                }
+
+                @Test
+                public void ludiiBoardIndex() {
+                    assertFalse(
+                        new LudiiMoves(LudiiContext.forNewGame())
+                            .availableMoveBy(
+                                new LudiiBoardIndex(new ActionMove(51, 51))
+                            )
+                            .isPresent()
+                    );
+                }
+            }
+        }
+    }
+}
