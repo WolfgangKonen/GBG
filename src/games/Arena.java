2559aa516a45a443dd6391aff83416e594189278
Tez90 <Tim.Zeh90@gmail.com>
2020-11-10 12:08:29 +0100

    added case for four players in arena for the game over message

--- src/games/Arena.java
+++ src/games/Arena.java
@@ -674,8 +674,10 @@ abstract public class Arena implements Runnable {
 						break; 
 					case 3:
 						showMessage(gostr, "Game Over", JOptionPane.INFORMATION_MESSAGE);
-	
-						break; 
+						break;
+					case 4:
+						showMessage(gostr, "Game Over", JOptionPane.INFORMATION_MESSAGE);
+						break;
 					default:
 						throw new RuntimeException("Case numPlayers = "+numPlayers+" not handled!");
 					}
