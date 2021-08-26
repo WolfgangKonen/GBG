76c885f291572b1cdf157493d4051b4f7b66cc7d
WolfgangKonen <WolfgangKonen@users.noreply.github.com>
2021-08-26 11:30:37 +0200

    EWN: Token & Player serializable
    ... to enable logging

--- src/games/EWN/StateObserverHelper/Token.java
+++ src/games/EWN/StateObserverHelper/Token.java
@@ -2,9 +2,10 @@ package games.EWN.StateObserverHelper;
 
 import tools.Types;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Token {
+public class Token implements Serializable {
     private ArrayList<Types.ACTIONS> availableActions;
     private int index;
     private int value;
