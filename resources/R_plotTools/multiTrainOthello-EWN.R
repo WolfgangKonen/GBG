f7c32cdbf31fc2031b859df0f98a0cb46e3f3a19
WolfgangKonen <WolfgangKonen@users.noreply.github.com>
2023-05-09 18:48:36 +0200

    EWN - master check

---
+++ resources/R_plotTools/multiTrainOthello-EWN.R
@@ -0,0 +1,74 @@
+#
+# **** These are May'2023 results with MCTS-wrapped TDNTuple4Agt on Othello. 
+#      This is just a check that the new EWN-Sarsa4 branch and the older master branch
+#      deliver statistically the same results on Othello
+#         EWN:      branch EWN-Sarsa4, 9 agents
+#         master:   branch master, 9 agents
+#      When evaluating against Edax, we use always 10.000 MCTS iterations
+# ****
+#
+# 
+library(ggplot2)
+library(grid)
+source("~/GitHub/GBG/resources/R_plotTools/summarySE.R")
+
+wfac = 1000;
+errWidth=0.2;
+xlabel = "Edax level"
+ylabel = "win rate"
+path <- "~/GitHub/GBG/agents/Othello/csv/"; 
+Ylimits=c(0.0,1.0); 
+Xlimits=factor(1:9); 
+
+pdffile=paste0("MCTSWrap-EWN-250k.May23.pdf")
+
+filenames=c(
+  "multi-EWN.csv",            # branch EWN-Sarsa4
+  "multi-EWN-master.csv"      # branch master
+  # generated with >GBGBatch Othello 6 ...
+)
+agroup = c("EWN","master")   
+
+dfAll = data.frame()
+for (k in 1:length(filenames)) {
+  filename <- paste0(path,filenames[k])
+  df <- read.csv(file=filename, skip=2, dec=".",sep=";")
+  df <- cbind(df,agentGroup=rep(agroup[k],nrow(df)))
+  dfAll <- rbind(dfAll,df)
+}
+
+dfAll <- dfAll[dfAll$iterMWrap==10000,]
+
+
+tgc <- data.frame()
+# summarySE is a very useful script from www.cookbook-r.com/Graphs/Plotting_means_and_error_bars_(ggplot2)
+# It summarizes a dataset, by grouping measurevar according to groupvars and calculating
+# its mean, sd, se (standard dev of the mean), ci (conf.interval) and count N.
+tgc <- summarySE(dfAll, measurevar="winrate", groupvars=c("dEdax","EPS","agentGroup")) 
+tgc$EPS <- as.factor(tgc$EPS)
+tgc <- tgc[tgc$EPS==1e-8,]
+#tgc <- tgc[tgc$EPS==0,]
+#tgc <- tgc[tgc$EPS==-1e-8,]
+
+# If errorbars overlap, use position_dodge to move them horizontally
+#pd <- position_dodge(1000/wfac) # move them 1000/wfac to the left and right
+
+
+q <- ggplot(tgc,aes(x=dEdax,y=winrate,colour=agentGroup,shape=agentGroup)) #,linetype=agentGroup))
+#q <- q+geom_errorbar(aes(ymin=winrate-se, ymax=winrate+se), width=errWidth) #, position=pd)
+q <- q+labs(title=paste("250,000 training episodes ")) 
+#q <- q+geom_line(position=pd,size=1.0) + geom_point(position=pd,size=2.0) 
+q <- q+geom_line(size=1.0) + geom_point(size=3.0)
+q <- q+scale_x_discrete(limits=Xlimits) 
+q <- q+scale_y_continuous(limits=Ylimits) + xlab(xlabel) + ylab(ylabel)
+q <- q+guides(colour = guide_legend(reverse = F))
+q <- q+guides(shape = guide_legend(reverse = F))
+q <- q+guides(linetype = guide_legend(reverse = F))
+q <- q+theme(axis.title = element_text(size = rel(1.5)))    # bigger axis labels 
+q <- q+theme(axis.text = element_text(size = rel(1.5)))     # bigger tick mark text  
+q <- q+theme(legend.text = element_text(size = rel(1.2)))   # bigger legend text  
+q <- q+theme(plot.title = element_text(size = rel(1.5)))    # bigger title text  
+
+plot(q)
+ggsave(pdffile, width = 8.04, height = 4.95, units = "in")
+cat(paste("Plot saved to",pdffile))
